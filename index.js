82fd5802f6a39735889d696d6093227042b24f58
webster <wbian618@gmail.com>
2021-10-13 21:20:08 -0400

    commit tree eb1b3fa4134baa791bd64fc244826503cd497ff5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    return arrayOfDrivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(arrayOfDrivers) {
+    return arrayOfDrivers.slice(2, arrayOfDrivers.length)
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+ function createFareMultiplier(mult) {
+     return function (fare) {
+         return mult * fare
+     }
+ }
+ const fareDoubler = createFareMultiplier(2)
+
+ const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, diffDrivers) {
+    return diffDrivers(arrayOfDrivers)
+}
